431c560eef001c1ca77fbf2973a149ce0c195a95
Wee Seng <wee.bscl@gmail.com>
2023-02-03 16:53:26 +0800

    init commit

---
+++ src/main/java/nus/iss/tfip/pafpizzarest/PafPizzarestApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafpizzarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafPizzarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafPizzarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/tfip/pafpizzarest/PafPizzarestApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafpizzarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafPizzarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
