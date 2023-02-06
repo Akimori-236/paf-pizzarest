c359e5e812e16391ccf00b9e9f85b36765612ae8
Wee Seng <wee.bscl@gmail.com>
2023-02-07 05:06:53 +0800

    utils

---
+++ src/main/java/nus/iss/tfip/pafpizzarest/util/PizzaUtil.java
@@ -0,0 +1,23 @@
+package nus.iss.tfip.pafpizzarest.util;
+
+import java.util.List;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
+public class PizzaUtil {
+    public static String notFoundJson(String orderId) {
+        JsonObject jObj = Json.createObjectBuilder()
+                .add("message", "Order %s not found".formatted(orderId))
+                .build();
+        return jObj.toString();
+    }
+
+    public static String getValidationError(BindingResult binding) {
+        List<ObjectError> errors = binding.getAllErrors();
+        return errors.get(0).getDefaultMessage().toString();
+    }
+}
